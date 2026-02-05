25ec0525d6b9fa2c6818d71a6cd7c1f1472a5cf3
ultimatemaggot <lizaengel32@gmail.com>
2026-02-05 15:07:56 +0200

    Implement priority selection in task list creation
    Added priority selection functionality and updated task list creation logic.

--- download_2026-02-05_14-05-37/zemturis-ingus_110_todo-list-V1.js
+++ download_2026-02-05_14-05-37/zemturis-ingus_110_todo-list-V1.js
@@ -1,9 +1,33 @@
+let izveletaPriorit = "Vidēja";	
+
+document.querySelectorAll("#prioritates option").forEach(item => {
+    item.onclick = function (e) {
+        e.preventDefault();
+        izveletaPrioritate = this.value;
+    };
+});
+
 function listCreation (){
+  
                 var textData = document.getElementById("textInput").value;
                 if (textData == ""){
                     return;
                 }
                 var ul = document.getElementById("task-list");
+                const prioritate = izveletaPrioritate;
+	
+              	const prioritIndikator = document.createElement('span');
+              	prioritIndikator.textContent = prioritate + " ";
+                if (prioritate = "Svarīgi") {
+                  prioritIndikator.className = "bg-danger";  // !
+                }
+              	
+              	const checkb = document.createElement('input');
+              	checkb.type = "checkbox";
+              	
+              	checkb.onclick = function () {
+                  textContent.classList.toggle("classList.toggle");
+              	};
                 var listItem = document.createElement("li");
                 var listBgone = document.createElement("button");
                 listBgone.addEventListener("click", function (){
