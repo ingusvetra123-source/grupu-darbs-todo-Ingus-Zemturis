e20ed43af01ca99b5718969a7254210f7cf12b28
ultimatemaggot <lizaengel32@gmail.com>
2026-02-06 15:01:58 +0200

    Kļūdu labošana + localStorage pievienošana

--- download_2026-02-05_14-05-37/zemturis-ingus_110_todo-list-V1.js
+++ download_2026-02-05_14-05-37/zemturis-ingus_110_todo-list-V1.js
@@ -1,42 +1,59 @@
-let izveletaPriorit = "Vidēja";	
-
-document.querySelectorAll("#prioritates option").forEach(item => {
-    item.onclick = function (e) {
-        e.preventDefault();
-        izveletaPrioritate = this.value;
-    };
-});
-
 function listCreation (){
   
-                var textData = document.getElementById("textInput").value;
+                let textData = document.getElementById("textInput").value;
                 if (textData == ""){
                     return;
                 }
-                var ul = document.getElementById("task-list");
-                const prioritate = izveletaPrioritate;
-	
-              	const prioritIndikator = document.createElement('span');
-              	prioritIndikator.textContent = prioritate + " ";
-                if (prioritate = "Svarīgi") {
-                  prioritIndikator.className = "bg-danger";  // !
-                }
-              	
-              	const checkb = document.createElement('input');
+				
+                let ul = document.getElementById("task-list");
+				
+                let listItem = document.createElement("li");
+				
+		// checkbox
+				const checkb = document.createElement('input');
               	checkb.type = "checkbox";
               	
               	checkb.onclick = function () {
-                  textContent.classList.toggle("classList.toggle");
+                  listItem.classList.toggle("text-decoration-line-through");
               	};
-                var listItem = document.createElement("li");
-                var listBgone = document.createElement("button");
+		// prioritate
+				let prioritates = document.getElementById('prioritates');
+				let prioritVertiba = prioritates.value;
+		
+              	const prioritIndikator = document.createElement('span');
+              	prioritIndikator.textContent = prioritVertiba + " ";
+				
+                if (prioritVertiba === "Svarīgi") {
+                  prioritIndikator.className = "badge badge-pill bg-danger";
+                }
+				else if (prioritVertiba === "Vidēja") {
+                  prioritIndikator.className = "badge badge-pill bg-warning";
+                }
+				else {
+				  prioritIndikator.className = "badge badge-pill bg-success";
+				}
+				
+                let listBgone = document.createElement("button");
                 listBgone.addEventListener("click", function (){
                     listItem.remove();
                 });
+				
                 listBgone.className = "btn btn-danger";
                 listBgone.innerHTML = "dzēst";
                 listItem.className = "list-group-item d-flex justify-content-between align-items-center";
                 listItem.innerHTML = textData;
+				listItem.appendChild(prioritIndikator);
+				listItem.appendChild(checkb);
                 listItem.appendChild(listBgone);
                 ul.appendChild(listItem);
             }
+function saveData() {
+	let data = document.querySelectorAll('li');
+	localStorage.setItem('data', JSON.stringify(data));
+}
+
+function loadData() {
+	let dataRetrieve = localStorage.getItem('data');
+}
+
+loadData();
